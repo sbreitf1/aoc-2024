9e648e5442f1813e4568cc58d6bd8ba92a20c331
Simon Breitfelder <breitfelder@denic.de>
2024-12-09 10:47:23 +0100

    puzzle 9, part 2

--- puzzle-9/main.go
+++ puzzle-9/main.go
@@ -6,159 +6,161 @@ import (
 	"aoc/helper"
 	"fmt"
 	"sort"
+	"strings"
 )
 
 func main() {
-	str := helper.ReadString("example-1.txt")
+	str := helper.ReadString("input.txt")
 	disk := parseDisk(str)
 
 	disk1 := disk.Clone()
-	disk1.Compact1()
+	disk1.SplitBlocks()
+	disk1.Compact()
 	solution1 := disk1.Checksum()
 	fmt.Println("-> part 1:", solution1)
 
 	disk2 := disk.Clone()
-	disk2.Compact2()
+	disk2.Compact()
 	solution2 := disk2.Checksum()
-	fmt.Println("-> part 2:", solution2, "(MAYBE WRONG!)")
-
-	// 8648109921625 is too high
-	// 6458577412415 is too low
+	fmt.Println("-> part 2:", solution2)
 }
 
 const (
 	FileIDNone FileID = -1
 )
 
-type FileID int64
+type FileID int
 
-type File struct {
-	ID   FileID
+type Block struct {
 	Pos  int
 	Size int
+	ID   FileID
 }
 
 type Disk struct {
-	blocks                  []FileID
-	files                   []File
-	leftmostEmptySpaceIndex int
-	emptySpaces             map[int][]int
-	maxEmptySpaceLen        int
+	fileBlocks  []Block
+	emptyBlocks []Block
+	size        int
+}
+
+func (d *Disk) String() string {
+	runes := []rune(strings.Repeat(" ", d.size))
+	for _, b := range d.fileBlocks {
+		for i := 0; i < b.Size; i++ {
+			runes[b.Pos+i] = '0' + rune(b.ID%10)
+		}
+	}
+	for _, b := range d.emptyBlocks {
+		for i := 0; i < b.Size; i++ {
+			runes[b.Pos+i] = '.'
+		}
+	}
+	return string(runes)
 }
 
 func (d *Disk) Clone() *Disk {
 	return &Disk{
-		blocks:                  helper.CloneSlice(d.blocks),
-		files:                   helper.CloneSlice(d.files),
-		leftmostEmptySpaceIndex: d.leftmostEmptySpaceIndex,
-		emptySpaces:             helper.CloneMapOfSlices(d.emptySpaces),
-		maxEmptySpaceLen:        d.maxEmptySpaceLen,
+		fileBlocks:  helper.CloneSlice(d.fileBlocks),
+		emptyBlocks: helper.CloneSlice(d.emptyBlocks),
+		size:        d.size,
 	}
 }
 
 func parseDisk(str string) *Disk {
-	blocks := make([]FileID, 0)
-	files := make([]File, 0)
-	emptySpaces := make(map[int][]int)
-	var maxEmptySpaceLen int
+	fileBlocks := make([]Block, 0)
+	emptyBlocks := make([]Block, 0)
+	var pos int
 	for i, r := range str {
 		if r >= '0' && r <= '9' {
 			blockLen := int(r - '0')
-			fileID := FileIDNone
 			if i%2 == 0 {
-				fileID = FileID(i / 2)
-				files = append(files, File{
-					ID:   fileID,
-					Pos:  len(blocks),
+				fileBlocks = append(fileBlocks, Block{
+					Pos:  pos,
 					Size: blockLen,
+					ID:   FileID(i / 2),
 				})
+
 			} else {
 				if blockLen > 0 {
-					if blockLen > maxEmptySpaceLen {
-						maxEmptySpaceLen = blockLen
-					}
-					emptySpaces[blockLen] = append(emptySpaces[blockLen], len(blocks))
+					emptyBlocks = append(emptyBlocks, Block{
+						Pos:  pos,
+						Size: blockLen,
+						ID:   FileIDNone,
+					})
 				}
 			}
-			for j := 0; j < blockLen; j++ {
-				blocks = append(blocks, fileID)
-			}
+
+			pos += blockLen
 		}
 	}
 	return &Disk{
-		blocks:                  blocks,
-		files:                   files,
-		leftmostEmptySpaceIndex: -1,
-		emptySpaces:             emptySpaces,
-		maxEmptySpaceLen:        maxEmptySpaceLen,
+		fileBlocks:  fileBlocks,
+		emptyBlocks: emptyBlocks,
+		size:        pos,
 	}
 }
 
-func (d *Disk) Compact1() {
-	for i := len(d.blocks) - 1; i >= 0; i-- {
-		d.selectNextEmptySpaceIndex()
-
-		if d.leftmostEmptySpaceIndex >= i {
-			break
-		}
-
-		if d.blocks[i] != FileIDNone {
-			d.blocks[d.leftmostEmptySpaceIndex] = d.blocks[i]
-			d.blocks[i] = FileIDNone
+func (d *Disk) SplitBlocks() {
+	splittedFiles := make([]Block, 0)
+	for _, f := range d.fileBlocks {
+		for i := 0; i < f.Size; i++ {
+			splittedFiles = append(splittedFiles, Block{
+				Pos:  f.Pos + i,
+				Size: 1,
+				ID:   f.ID,
+			})
 		}
 	}
+	d.fileBlocks = splittedFiles
 }
 
-func (d *Disk) selectNextEmptySpaceIndex() {
-	for d.leftmostEmptySpaceIndex < len(d.blocks) {
-		if d.leftmostEmptySpaceIndex >= 0 && d.blocks[d.leftmostEmptySpaceIndex] == FileIDNone {
-			return
+func (d *Disk) Compact() {
+	for i := len(d.fileBlocks) - 1; i >= 0; i-- {
+		if d.fileBlocks[i].Size == 0 {
+			// nothing to move
+			continue
 		}
-		d.leftmostEmptySpaceIndex++
+
+		emptyIndex, ok := d.findEmptySpaceBlockIndex(d.fileBlocks[i].Size)
+		if !ok {
+			// cannot move
+			continue
+		}
+
+		if d.emptyBlocks[emptyIndex].Pos >= d.fileBlocks[i].Pos {
+			// already leftmost index
+			continue
+		}
+
+		d.fileBlocks[i].Pos = d.emptyBlocks[emptyIndex].Pos
+		if d.emptyBlocks[emptyIndex].Size > d.fileBlocks[i].Size {
+			d.emptyBlocks[emptyIndex].Pos += d.fileBlocks[i].Size
+			d.emptyBlocks[emptyIndex].Size -= d.fileBlocks[i].Size
+		} else {
+			d.emptyBlocks = helper.RemoveIndex(d.emptyBlocks, emptyIndex)
+		}
 	}
-	panic("no empty space left")
+
+	sort.Slice(d.fileBlocks, func(i, j int) bool {
+		return d.fileBlocks[i].Pos < d.fileBlocks[j].Pos
+	})
+}
+
+func (d *Disk) findEmptySpaceBlockIndex(blockLen int) (int, bool) {
+	for i := range d.emptyBlocks {
+		if d.emptyBlocks[i].Size >= blockLen {
+			return i, true
+		}
+	}
+	return -1, false
 }
 
 func (d *Disk) Checksum() int64 {
 	var checksum int64
-	for i, f := range d.blocks {
-		if f != FileIDNone {
-			checksum += int64(i) * int64(f)
+	for _, f := range d.fileBlocks {
+		for i := 0; i < f.Size; i++ {
+			checksum += int64((f.Pos + i) * int(f.ID))
 		}
 	}
 	return checksum
-}
-
-func (d *Disk) Compact2() {
-	for i := len(d.files) - 1; i >= 0; i-- {
-		pos, emptyBlockLen, ok := d.findFittingEmptySpace(d.files[i].Size)
-		if ok {
-			if pos >= d.files[i].Pos {
-				continue
-			}
-
-			for j := 0; j < d.files[i].Size; j++ {
-				d.blocks[pos+j] = d.files[i].ID
-				d.blocks[d.files[i].Pos+j] = FileIDNone
-			}
-			d.files[i].Pos = pos
-
-			d.emptySpaces[emptyBlockLen] = d.emptySpaces[emptyBlockLen][1:]
-			remainingBlockLen := emptyBlockLen - d.files[i].Size
-			if remainingBlockLen > 0 {
-				d.emptySpaces[remainingBlockLen] = append(d.emptySpaces[remainingBlockLen], pos+d.files[i].Size)
-				sort.Ints(d.emptySpaces[remainingBlockLen])
-				fmt.Println(remainingBlockLen, "->", d.emptySpaces[remainingBlockLen])
-			}
-		}
-	}
-}
-
-func (d *Disk) findFittingEmptySpace(blockLen int) (int, int, bool) {
-	for i := blockLen; i <= d.maxEmptySpaceLen; i++ {
-		if arr, ok := d.emptySpaces[i]; ok && len(arr) > 0 {
-			return arr[0], i, true
-		}
-	}
-	return -1, -1, false
 }
