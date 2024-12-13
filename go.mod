f6640e846b4ee0eb9dbbb1b7779a2bc0f50a09f6
Simon Breitfelder <breitfelder@denic.de>
2024-12-13 12:31:04 +0100

    puzzle 13, part 2

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module aoc
 
 go 1.23
 
-require github.com/stretchr/testify v1.10.0
+require (
+	github.com/alex-ant/gomath v0.0.0-20160516115720-89013a210a82
+	github.com/stretchr/testify v1.10.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
--- puzzle-13/main.go
+++ puzzle-13/main.go
@@ -6,6 +6,9 @@ import (
 	"aoc/helper"
 	"fmt"
 	"strings"
+
+	"github.com/alex-ant/gomath/gaussian-elimination"
+	"github.com/alex-ant/gomath/rational"
 )
 
 func main() {
@@ -14,19 +17,23 @@ func main() {
 
 	solution1 := SumMinTokens(machines)
 	fmt.Println("-> part 1:", solution1)
+
+	machines2 := ConvertMachinesToPart2(machines)
+	solution2 := SumMinTokens(machines2)
+	fmt.Println("-> part 2:", solution2)
 }
 
 type ClawMachine struct {
-	ButtonA, ButtonB helper.Vec2D[int]
-	Prize            helper.Vec2D[int]
+	ButtonA, ButtonB helper.Vec2D[int64]
+	Prize            helper.Vec2D[int64]
 }
 
 func ParseClawMachines(lines []string) []ClawMachine {
 	machines := make([]ClawMachine, 0)
-	var buttonA, buttonB, prize helper.Vec2D[int]
+	var buttonA, buttonB, prize helper.Vec2D[int64]
 	for _, l := range lines {
-		ints := helper.ExtractInts[int](l)
-		p := helper.Vec2D[int]{X: ints[0], Y: ints[1]}
+		ints := helper.ExtractInts[int64](l)
+		p := helper.Vec2D[int64]{X: ints[0], Y: ints[1]}
 		if strings.HasPrefix(l, "Button A:") {
 			buttonA = p
 		} else if strings.HasPrefix(l, "Button B:") {
@@ -43,8 +50,8 @@ func ParseClawMachines(lines []string) []ClawMachine {
 	return machines
 }
 
-func SumMinTokens(machines []ClawMachine) int {
-	var sum int
+func SumMinTokens(machines []ClawMachine) int64 {
+	var sum int64
 	for _, m := range machines {
 		cost, _ := m.GetMinTokens()
 		sum += cost
@@ -52,39 +59,41 @@ func SumMinTokens(machines []ClawMachine) int {
 	return sum
 }
 
-func (m ClawMachine) GetMinTokens() (int, bool) {
-	type ButtonPresses struct {
-		A, B int
-		Cost int
-		Pos  helper.Vec2D[int]
+func (m ClawMachine) GetMinTokens() (int64, bool) {
+	// https://github.com/alex-ant/gomath
+
+	nr := func(i int64) rational.Rational {
+		return rational.New(i, 1)
 	}
 
-	queue := helper.NewPriorityQueue[int, ButtonPresses]()
-	queue.Push(0, ButtonPresses{A: 0, B: 0, Cost: 0, Pos: helper.Vec2D[int]{X: 0, Y: 0}})
-	visited := make(map[ButtonPresses]bool)
+	equations := make([][]rational.Rational, 2)
+	equations[0] = []rational.Rational{nr(m.ButtonA.X), nr(m.ButtonB.X), nr(m.Prize.X)}
+	equations[1] = []rational.Rational{nr(m.ButtonA.Y), nr(m.ButtonB.Y), nr(m.Prize.Y)}
 
-	for queue.Len() > 0 {
-		bp, _ := queue.Pop()
-		if visited[bp] {
-			continue
-		}
-		visited[bp] = true
-
-		if bp.Pos == m.Prize {
-			return bp.Cost, true
-		}
-
-		if bp.A > 100 || bp.B > 100 {
-			continue
-		}
-		if bp.Pos.X > m.Prize.X || bp.Pos.Y > m.Prize.Y {
-			continue
-		}
-
-		bpa := ButtonPresses{A: bp.A + 1, B: bp.B, Cost: bp.Cost + 3, Pos: helper.Vec2D[int]{X: bp.Pos.X + m.ButtonA.X, Y: bp.Pos.Y + m.ButtonA.Y}}
-		queue.Push(bpa.Cost, bpa)
-		bpb := ButtonPresses{A: bp.A, B: bp.B + 1, Cost: bp.Cost + 1, Pos: helper.Vec2D[int]{X: bp.Pos.X + m.ButtonB.X, Y: bp.Pos.Y + m.ButtonB.Y}}
-		queue.Push(bpb.Cost, bpb)
+	res, err := gaussian.SolveGaussian(equations, false)
+	if err != nil {
+		panic(err)
 	}
-	return 0, false
+
+	if len(res) != 2 {
+		return 0, false
+	}
+	for _, v := range res {
+		if len(v) != 1 || !v[0].IsNatural() {
+			return 0, false
+		}
+	}
+
+	a := res[0][0].GetNumerator() / res[0][0].GetDenominator()
+	b := res[1][0].GetNumerator() / res[1][0].GetDenominator()
+
+	return 3*a + b, false
+}
+
+func ConvertMachinesToPart2(m []ClawMachine) []ClawMachine {
+	m2 := helper.Clone(m)
+	for i := range m2 {
+		m2[i].Prize = m2[i].Prize.Add(helper.Vec2D[int64]{X: 10000000000000, Y: 10000000000000})
+	}
+	return m2
 }
